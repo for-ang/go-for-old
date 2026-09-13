3c790adaa2ac843a8086be4c419894a32cfebd27
for-ang <ivyroseforst@gmail.com>
2026-09-13 12:38:06 +0100

    added shooting mechanic

--- scripts/crosshair.gd
+++ scripts/crosshair.gd
@@ -1,24 +1,27 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
+const SPEED = 200
+var shaketarget = Vector2i(80,72)
 
 func _physics_process(_delta: float):
-				#REDO THIS
-				#ITS BAD
+	velocity = position.direction_to(shaketarget) * SPEED
+	if position.distance_to(shaketarget) > 10:
+		move_and_slide()
+
+		#dont look at this
+		#it works but its disgusting
 	var directionx := Input.get_axis("LEFT", "RIGHT")
 	if directionx:
 		velocity.x = directionx * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
 	var directiony := Input.get_axis("UP", "DOWN")
 	if directiony:
 		velocity.y = directiony * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-
 	move_and_slide()
 
 
-func _on_shake_timer_timeout() -> void:
-	position = Vector2i(randi_range(20,140),randi_range(5,125))
+func _on_shake_timer_timeout():
+	shaketarget = Vector2i(randi_range(20,140),randi_range(5,125))
--- scripts/shooting.gd
+++ scripts/shooting.gd
@@ -1,15 +1,32 @@
 extends Node2D
 
 var intarget = 0
+var bullets = 3
+var score = 0 
 
 func _ready():
 	$crosshair.position = Vector2(80,72)
-
-func _process(delta: float) -> void:
-	print(intarget)
 	
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	intarget = 1
+func _process(_delta: float):
+	if bullets > 0:
+		if Input.is_action_just_pressed("A"):
+			score += intarget
+			bullets -= 1 
+	$ScoreLabel.text = str(score)
+	$BulletsLabel.text = str(bullets)
 
-func _on_area_2d_body_exited(body: Node2D) -> void:
-	intarget = 0 
+
+func _on_area_2d_body_entered(_body: Node2D):
+	intarget += 1
+func _on_area_2d_body_exited(_body: Node2D):
+	intarget -= 1 
+
+func _on_mid_body_entered(_body: Node2D):
+	intarget += 1
+func _on_mid_body_exited(_body: Node2D):
+	intarget -= 1
+
+func _on_outer_body_entered(_body: Node2D):
+	intarget += 1
+func _on_outer_body_exited(_body: Node2D):
+	intarget -= 1 
